1bba040f1f70cd2ea30319114360af49e8f58acb
Jaggy Gauran <jaggy@artisan.studio>
2020-02-08 21:57:58 +0800

    Add a sample response of the Semaphore API when successful.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,4 +4,8 @@ namespace Tests;
 
 class TestCase extends \Orchestra\Testbench\TestCase
 {
+    protected function getPackageProviders($app)
+    {
+        return [\Humans\Semaphore\ServiceProvider::class];
+    }
 }
