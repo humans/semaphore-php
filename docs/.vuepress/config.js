20bd9a730c0d956f1289472d8a1051ba30baa70c
Jaggy Gauran <jaggy@artisan.studio>
2020-02-09 13:22:26 +0800

    Set up vuepress.

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  themeConfig: {
+    sidebar: [
+      {
+        title: 'Guide',
+        path: '/guide/',
+        children: [
+          ['/guide/', 'Introduction']
+        ],
+      }
+    ]
+  }
+}
